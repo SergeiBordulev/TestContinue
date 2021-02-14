15afedb064b1861b95913e34a5448bbc449295c5
SergeyBordulev <bordulev@yahoo.com>
2021-02-14 23:12:33 +0700

    Initial commit

---
+++ src/TestContinue.java
@@ -0,0 +1,42 @@
+public class TestContinue {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 100; i++) {
+            if (i == 5 || i % 3 == 0 || (i >= 60 && i <= 80)) {
+                continue;
+            }
+
+            System.out.print(i + " ");
+        }
+
+        System.out.println();
+
+        for (int i = 0; i <= 100; i++) {
+            if (i == 5) {
+                continue;
+            }
+
+            if (i % 3 == 0) {
+                continue;
+            }
+
+            if (i >= 60 && i <= 80) {
+                continue;
+            }
+
+            System.out.print(i + " ");
+        }
+
+        System.out.println();
+
+        int i = 0;
+        while (i <= 100) {
+            if (i == 5 || i % 3 == 0 || (i >= 60 && i <= 80)) {
+                ++i;
+                continue;
+            }
+
+            System.out.print(i + " ");
+            ++i;
+        }
+    }
+}
